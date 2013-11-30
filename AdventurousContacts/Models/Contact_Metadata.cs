001a5a6180438ac378293feab00a36e8e2db795a
Rickard Karlsson <sunnre3@gmail.com>
2013-11-30 03:22:17 +0100

    commit #1
    EF classes & metadata added

---
+++ AdventurousContacts/Models/Contact_Metadata.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AdventurousContacts.Models
+{
+	public class Contact_Metadata
+	{
+		[Required]
+		[MaxLength(50)]
+		[EmailAddress]
+		public string EmailAddress { get; set; }
+
+		[Required]
+		[MaxLength(50)]
+		public string FirstName { get; set; }
+
+		[Required]
+		[MaxLength(50)]
+		public string LastName { get; set; }
+	}
+}
